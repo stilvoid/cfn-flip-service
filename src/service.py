f41e52f4b03fe4e605205450e64d032cb9fcaaf4
Steve Engledow <steve@offend.me.uk>
2017-11-07 13:22:44 +0000

    Add additional debugging and fix header name

--- src/service.py
+++ src/service.py
@@ -8,30 +8,51 @@ CONTENT_TYPE_JSON = "application/json"
 CONTENT_TYPE_YAML = "application/x-yaml"
 
 def from_json(json):
+    try:
+        yaml = cfn_flip.to_yaml(json)
+    except Exception as e:
+        return user_error(e.message)
+
     return {
         "headers": {
             "Content-Type": CONTENT_TYPE_YAML,
         },
-        "body": cfn_flip.to_yaml(json),
+        "body": yaml,
     }
 
 def from_yaml(yaml):
+    try:
+        json = cfn_flip.to_json(yaml)
+    except Exception as e:
+        return user_error(e.message)
+
     return {
         "headers": {
             "Content-Type": CONTENT_TYPE_JSON,
         },
-        "body": cfn_flip.to_json(yaml),
+        "body": json,
+    }
+
+def user_error(message):
+    return {
+        "statusCode": 400,
+        "body": message,
     }
 
 def handler(event, context):
-    content_type = event["headers"]["Content-Type"]
+    print(event)
+    
+    if "content-type" not in event["headers"]:
+        return user_error("Missing Content-Type header")
 
+    if not event["body"]:
+        return user_error("Missing body")
+        
+    content_type = event["headers"]["content-type"]
+    
     if content_type == CONTENT_TYPE_JSON:
         return from_json(event["body"])
     elif content_type == CONTENT_TYPE_YAML:
         return from_yaml(event["body"])
 
-    return {
-        "statusCode": 400,
-        "body": "Unknown Content-Type: {}".format(content_type)
-    }
+    return user_error("Unknown Content-Type: {}".format(content_type))
